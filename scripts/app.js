28f4351d0c5c2b7e7e7e4c12c5546c7c8f5c3f51
Maksim Aisin <maksim.aisin@mail.ru>
2022-01-02 14:38:36 +0300

    feat: add scroll functionality

--- scripts/app.js
+++ scripts/app.js
@@ -34,4 +34,17 @@ window.onload = () => {
             modal.classList.remove('open-modal')
         }
     })
+
+    // anchor smooth scrolling
+    const anchors = document.querySelectorAll('a[href*="#"]')
+    for (let anchor of anchors) {
+        anchor.addEventListener('click', (e) => {
+            e.preventDefault()
+            const blockID = anchor.getAttribute('href')
+            document.querySelector('' + blockID).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start',
+            })
+        })
+    }
 }
